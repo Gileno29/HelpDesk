68c250e2db744448b0e3d030a530f97653a9be3c
Gileno <gileno.cordeiroc@gmail.com>
2020-01-09 15:49:59 -0300

    Finalização da visalização de chamados

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -2,6 +2,24 @@
  require_once('validador_de_acesso.php');
 ?>
 
+<?php  
+  $arquivo= fopen('chamado.hd','r');
+  $chamados;
+
+  while(!feof($arquivo)){//le até o final do arquivo
+    $registro=fgets($arquivo);
+    $chamados[]=$registro;
+
+
+  }
+
+//fecha o arquivo aberto
+  fclose($arquivo);
+  /*echo "<pre>";
+  print_r($chamados);
+  echo "</pre>";*/
+?>
+
 <html>
   <head>
     <meta charset="utf-8" />
@@ -37,21 +55,24 @@
             </div>
             
             <div class="card-body">
-              
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
+              <? foreach($chamados as $chamado) { ?>
+                <?php 
+                  $dados_chamado= explode("#", $chamado);
+                  if(count($dados_chamado)<3){
+                    continue;
+                  }
+                  ?>       
+                        <div class='card mb-3 bg-light'>
+                          <div class="card-body">
+                            <h5 class="card-title"><?=$dados_chamado[0]?></h5>
+                            <h6 class="card-subtitle mb-2 text-muted"><?= $dados_chamado[1]?></h6>
+                            <p class="card-text"><?= $dados_chamado[2] ?></p>
 
-                </div>
-              </div>
-
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
+                      </div>
+                    </div> 
+            
+            
+              <? } ?>
 
                 </div>
               </div>
